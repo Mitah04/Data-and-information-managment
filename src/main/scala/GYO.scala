b89616187d19fa2d22cfcc3a2e97c0d2ecf7e0ac
{{your_name}} <hatimdraoui@outlook.com>
2025-08-11 19:59:47 +0200

    Final isAnEar

--- src/main/scala/GYO.scala
+++ src/main/scala/GYO.scala
@@ -4,20 +4,38 @@ object GyoAlgo {
 
   }
 
-  def isAnEar(query: Query, atom: Atom) : Either[Atom, Boolean] = {
-    val nonSharedEdges: List[String] =
-      atom.terms.filter(v => isContained(query, List(v), atom).isEmpty)
-    if (nonSharedEdges.length == atom.terms.length)
-      Right(true) 
+  /**
+   * Checks if a given atom is an "ear" in the context of a query.
+   *
+   * @param query The full query.
+   * @param atom  The atom to check.
+   * @return Either a Left(witness: Atom) if it's an ear with a witness,
+   *         or a Right(isEar: Boolean) where true means it's an "island" ear
+   *         and false means it's not an ear at all.
+   */
+  def isAnEar(query: Query, atom: Atom): Either[Atom, Boolean] = {
+    val atomVariables = atom.terms.toSet
+    val otherAtoms = query.body.filter(_ != atom)
+
+    val sharedVariables = atomVariables.filter { variable =>
+      otherAtoms.exists(_.terms.contains(variable))
+    }
+
+    if (sharedVariables.isEmpty) {
+      Right(true)
+    }
     else {
-      val witness = isWitness(query, atom.terms.diff(nonSharedEdges), atom)
-      if witness.isEmpty
-        Right(false)
-      else
-        Left(witness[0])
-        
+      val potentialWitnesses = otherAtoms.filter { otherAtom =>
+        sharedVariables.subsetOf(otherAtom.terms.toSet)
       }
 
+      if (potentialWitnesses.nonEmpty) {
+        Left(potentialWitnesses.head)
+      }
+      else {
+        Right(false)
+      }
+    }
   }
 
   def isContained(query: Query, terms: List[String], current: Atom): List[Atom] =
